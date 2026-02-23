5c918578597c46af2843f53e2d58fb1fb4fcb3c8
Eunjinshin <58281884+Eunjinshin@users.noreply.github.com>
2026-02-23 17:52:54 +0900

    Update apiClient.tsx

--- front_dashboard/src/api/apiClient.tsx
+++ front_dashboard/src/api/apiClient.tsx
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: '/api',
+  //baseURL: '/api',
+  baseURL: 'https://dashboard-s9vm.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
